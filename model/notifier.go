704dd54bffa604d63c87d2b883eb31bf109679d0
Vishnu Jayadevan <me@vishnujayadevan.com>
2022-11-23 09:56:40 +0530

    refactor:introduct provider for notifiers and rename notifier_type to type

--- model/notifier.go
+++ model/notifier.go
@@ -2,15 +2,42 @@ package model
 
 type NotifierType string
 
+const NotifierTypeWebhook NotifierType = "webhook"
+
+type NotifierProvider string
+
+const NotifierProviderDefault NotifierProvider = "default"
+
+func ValidateNotifierType(notifierType NotifierType) bool {
+	validNotifierTypes := []NotifierType{NotifierTypeWebhook}
+	for _, validNotifierType := range validNotifierTypes {
+		if validNotifierType == notifierType {
+			return true
+		}
+	}
+	return false
+}
+
+func ValidateNotifierProvider(notifierProvider NotifierProvider) bool {
+	validNotifierProviders := []NotifierProvider{NotifierProviderDefault}
+	for _, validNotifierProvider := range validNotifierProviders {
+		if validNotifierProvider == notifierProvider {
+			return true
+		}
+	}
+	return false
+}
+
 type Notifier struct {
-	ID           string       `gorm:"primary_key"`
-	NotifierType NotifierType `gorm:"not null"`
-	SubscriberID string       `gorm:"not null"`
-	Subscriber   Subscriber   `gorm:"foreignkey:SubscriberID"`
-	TenantID     string       `gorm:"not null"`
-	Tenant       Tenant       `gorm:"foreignkey:TenantID"`
-	Config       []byte       `gorm:"type:jsonb;not null"`
-	IsActive     bool         `gorm:"not null;default:true"`
+	ID           string           `gorm:"primary_key"`
+	Type         NotifierType     `gorm:"column:type;not null"`
+	Provider     NotifierProvider `gorm:"column:provider;not null"`
+	SubscriberID string           `gorm:"not null"`
+	Subscriber   Subscriber       `gorm:"foreignkey:SubscriberID"`
+	TenantID     string           `gorm:"not null"`
+	Tenant       Tenant           `gorm:"foreignkey:TenantID"`
+	Config       []byte           `gorm:"type:jsonb;not null"`
+	IsActive     bool             `gorm:"not null;default:true"`
 }
 type NotifierRepository interface {
 	Create(notifier *Notifier) error
--- service/notifier.go
+++ service/notifier.go
@@ -23,27 +23,33 @@ func NewNotifierService(
 type AddNotifierRequest struct {
 	SubscriberID string
 	TenantID     string
-	NotifierType model.NotifierType
+	Type         model.NotifierType
+	Provider     model.NotifierProvider
 	Config       []byte
 }
 
 // AddNotifier adds a notifier to a subscriber
-func (s *NotifierService) Create(request *AddNotifierRequest) error {
+func (s *NotifierService) Create(request *AddNotifierRequest) (*model.Notifier, error) {
 	subscriber, err := s.subscriberService.GetSubscriber(request.TenantID, request.SubscriberID)
 	if err != nil {
-		return err
+		return nil, err
 	}
+
 	notifier := &model.Notifier{
 		ID:           ksuid.New().String(),
 		Subscriber:   *subscriber,
 		SubscriberID: subscriber.ID,
 		TenantID:     subscriber.TenantID,
 		Tenant:       subscriber.Tenant,
-		NotifierType: request.NotifierType,
+		Type:         request.Type,
+		Provider:     request.Provider,
 		Config:       request.Config,
 	}
 
-	return s.notifierRepository.Create(notifier)
+	if err := s.notifierRepository.Create(notifier); err != nil {
+		return nil, err
+	}
+	return notifier, nil
 }
 
 func (s *NotifierService) Filter(subscriberID, tenantID string, notifierType model.NotifierType, isActive bool) ([]model.Notifier, error) {
